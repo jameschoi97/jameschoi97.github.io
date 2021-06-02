a37905ba1aeffe4c09eeab563e57888961c908fe
jameschoi97 <jameschoi97@nyu.edu>
2021-06-02 19:22:57 +0900

    Create WebApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:jameschoi97/main.dart';
+import 'package:jameschoi97/choi_webapp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(WebApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
